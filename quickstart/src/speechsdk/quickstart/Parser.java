95a8e3270009cff69dc3dbb4c570d1e7f9763500
Maria Pospelova <mariyapospelova@gmail.com>
2019-11-23 11:34:13 +0100

    parse javafile

--- quickstart/src/speechsdk/quickstart/Parser.java
+++ quickstart/src/speechsdk/quickstart/Parser.java
@@ -1,10 +1,12 @@
 package speechsdk.quickstart;
 
+import com.github.javaparser.ParseException;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public interface Parser {
 	
-	public String parseSyntax(String docpath) throws IOException;
+	public String parseSyntax(String docpath) throws IOException, ParseException;
 
 }
