0b92c9c463c7399b5fd5206227086b777b77b915
sutedalm <37000551+sutedalm@users.noreply.github.com>
2019-11-23 16:32:17 +0100

    remove weird kotlin module

--- ByteTheDust/src/ByteTheDust/Rubberdocs/FileChooseGUI.java
+++ ByteTheDust/src/ByteTheDust/Rubberdocs/FileChooseGUI.java
@@ -9,8 +9,10 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,18 +23,16 @@ public class FileChooseGUI extends Application implements EventHandler<ActionEve
         //SpeechToText speechToText=new SpeechToText();
         //speechToText.getUserInput();
     }
-
     @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FileChooseGUI.fxml"));
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(new URL("file:///C:/data/hello-world.fxml"));
+        VBox vbox = loader.<VBox>load();
 
-        Scene scene = new Scene(root, 600, 900);
-
-        stage.setTitle("FXML Welcome");
-        stage.setScene(scene);
-        stage.show();
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
-
     @Override
     public void handle(ActionEvent event) {
     }
