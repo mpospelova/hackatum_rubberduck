b8d3d20316863fe41107fb6cb7f1c1594f270266
Maria Pospelova <mariyapospelova@gmail.com>
2019-11-23 21:11:47 +0100

    now we can edit the text and save it

--- ByteTheDust/src/ByteTheDust/Rubberdocs/TestFile.java
+++ ByteTheDust/src/ByteTheDust/Rubberdocs/TestFile.java
@@ -6,10 +6,32 @@ public class TestFile {
 
 
 	//Hello Consti, This Method does Foo
+	//This is a function.
+	//Zoom out.
+	//Mode.
+	//Hello.
+	//Hello.
+	//This is the whole function.
+	//What is the cause of this is safe but and with who?
+	//Testing string function.
+	//Cocoa nut.
+	//Test foo function.
+
 	public String foo() {
 		return "Bar";
 	}
 
+	//This is the main function.Linda merges there is a gallon.At least.
+	//Hello World,
+	//Hello hello turn the radio on.
+	//Directly is comments to the source code source code.
+	//Test the safe button.Pause.
+	//This thing made function.
+	//Test Mail function.
+	//Test test test.
+	//This previous.
+	//Hello World,Hello Hello,
+	//Complete into dumpsters American.Yep.Testing testing
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
