a8bbb63f216738770c825376cbdd280078df7e84
Maria Pospelova <mariyapospelova@gmail.com>
2019-11-23 10:17:09 +0100

    remove function from interface

--- quickstart/src/speechsdk/quickstart/JavaParser.java
+++ quickstart/src/speechsdk/quickstart/JavaParser.java
@@ -5,14 +5,10 @@ public class JavaParser implements Parser{
   @Override
   public String parseSyntax(String docpath) {
     // TODO Auto-generated method stub
+
     return null;
   }
 
-  @Override
-  public void writeAnnotations(String key, String annotation) {
-    // TODO Auto-generated method stub
-
-  }
 
 
 }
--- quickstart/src/speechsdk/quickstart/Parser.java
+++ quickstart/src/speechsdk/quickstart/Parser.java
@@ -3,6 +3,5 @@ package speechsdk.quickstart;
 public interface Parser {
 	
 	public String parseSyntax(String docpath);
-	public void writeAnnotations(String key, String annotation);
 
 }
