c084c6b57ae2ceafc3872ec80d4fa84cf62c9123
lorenz3001 <LolloGit1+>
2019-11-24 05:47:16 +0100

    Improved optics for landing Page and Filesort page

--- ByteTheDust/src/ByteTheDust/Rubberdocs/TestFile.java
+++ ByteTheDust/src/ByteTheDust/Rubberdocs/TestFile.java
@@ -1,49 +1,14 @@
 package ByteTheDust.Rubberdocs;
 
 
-//Annotation1, (belong to class 1)
+
 public class TestFile {
 
 
-	//Hello Consti, This Method does Foo
-	//This is a function.
-	//Zoom out.
-	//Mode.
-	//Hello.
-	//Hello.
-	//This is the whole function.
-	//What is the cause of this is safe but and with who?
-	//Testing string function.
-	//Cocoa nut.
-	//Test foo function.
-
-	//This is the documentation for Maine.
-	//Hello test, 1234.
-	//Hello.
-	//test
-	//testadsfsdaasdsfaadsf
-	//Hello.
-	//Yeah, click music.
-	//About 10 ish make?
-	//So OK.
-	//HI
-	//Test documentation for foo.
-	//Let's document some functions for the method. Foo it takes several parameters for example, one that is stupid.
 	public String foo() {
 		return "Bar";
 	}
 
-	//This is the main function.Linda merges there is a gallon.At least.
-	//Hello World,
-	//Hello hello turn the radio on.
-	//Directly is comments to the source code source code.
-	//Test the safe button.Pause.
-	//This thing made function.
-	//Test Mail function.
-	//Test test test.
-	//This previous.
-	//Hello World,Hello Hello,
-	//Complete into dumpsters American.Yep.Testing testing
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
